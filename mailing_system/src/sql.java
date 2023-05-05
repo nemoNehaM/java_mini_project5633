d45b1fcf2895665dd79d287149fcf3c76de9f7b2
nemoNehaM <nehamathew@msds.christuniversity.in>
2023-05-05 18:19:28 +0530

    SQL FIle
    Java code for SQL is used to connect Java programs to databases and execute SQL commands to manipulate the data in those databases. In a Java SQL program, the first step is to establish a connection to the database using a JDBC driver. The driver is responsible for communicating with the database and translating Java calls into SQL commands that the database can understand, which are all written in the code

---
+++ mailing_system/src/sql.java
@@ -0,0 +1,60 @@
+import java.sql.*;
+
+public class sql {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test1"; 
+        String user = "root"; 
+        String password = "nehaniki123@J"; 
+        
+        try {
+            // Connect to the database
+            Connection connection = DriverManager.getConnection(url, user, password);
+            
+            // Create the Users table
+            String createUsersTable = "CREATE TABLE Users ("
+                    + "User_id INT PRIMARY KEY AUTO_INCREMENT,"
+                    + "username VARCHAR(50) NOT NULL,"
+                    + "email_address VARCHAR(100) NOT NULL,"
+                    + "password VARCHAR(255) NOT NULL,"
+                    + "phone_no VARCHAR(20),"
+                    + "datestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+                    + ")";
+            Statement usersStatement = connection.createStatement();
+            usersStatement.executeUpdate(createUsersTable);
+            System.out.println("Users table created successfully!");
+            
+            // Create the Inbox table
+            String createInboxTableQuery = "CREATE TABLE Inbox ("
+                                    + "sender_id INT NOT NULL,"
+                                    + "receiver_id INT NOT NULL,"
+                                    + "subject VARCHAR(255) NOT NULL,"
+                                    + "message TEXT NOT NULL,"
+                                    + "date_received DATETIME NOT NULL,"
+                                    + "FOREIGN KEY (sender_id) REFERENCES Users(user_id),"
+                                    + "FOREIGN KEY (receiver_id) REFERENCES Users(user_id)"
+                                    + ")";
+            Statement msgSendStatement = connection.createStatement();
+            msgSendStatement.executeUpdate(createInboxTableQuery);
+            System.out.println("Msg_send table created successfully!");
+            
+            // Create the Admin table
+            String createAdminTableQuery = "CREATE TABLE Admin ("
+                                    + "admin_id INT PRIMARY KEY AUTO_INCREMENT,"
+                                    + "admin_name VARCHAR(255) NOT NULL,"
+                                    + "admin_mail_address TEXT NOT NULL,"
+                                    + "admin_password VARCHAR(30) NOT NULL"
+                                    + ")";
+            Statement AdminStatement = connection.createStatement();
+            AdminStatement.executeUpdate(createAdminTableQuery);
+            System.out.println("Admin table created successfully!");
+            
+            
+            // Close the connection
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
